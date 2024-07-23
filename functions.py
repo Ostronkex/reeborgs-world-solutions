95eae13ff832573328891e3eaa64a991e2880239
Ostronkex <erikmagnusson.privat@gmail.com>
2024-07-23 02:11:04 +0200

    Add code to functions.py

---
+++ functions.py
@@ -0,0 +1,22 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+def move2():
+    move()
+    move()
+
+def move3():
+    move()
+    move()
+    move()
