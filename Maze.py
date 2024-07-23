bfd4a545e292c0658c4e939c664ced5a3a20705b
Ostronkex <erikmagnusson.privat@gmail.com>
2024-07-23 18:59:12 +0200

    Update Maze.py

--- Maze.py
+++ Maze.py
@@ -1,1 +1,5 @@
--
+    #The functions move() and turn_left().
+    #Either the test front_is_clear() or wall_in_front(), right_is_clear() or wall_on_right(), and at_goal().
+    #How to use a while loop and if/elif/else statements.
+    #It might be useful to know how to use the negation of a test (not in Python).
+
